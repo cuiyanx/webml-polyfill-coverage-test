ac63f56eb608909d09cff166761f58724a19c5ad
cuiyan <yanx.cui@intel.com>
2019-03-07 13:35:13 +0800

    Fix: appveyor build failed 5
    Signed-off-by: cuiyan <yanx.cui@intel.com>

--- test-coverage.js
+++ test-coverage.js
@@ -157,7 +157,7 @@ var driver, chromeOption, testURL, webmlpolyfillHost;
     console.log("webml-polyfill web host is start");
 
     if (os.type() == "Windows_NT") {
-        webmlpolyfillHost = childprocess.spawn("dir", {stdio: "inherit"});
+        webmlpolyfillHost = childprocess.spawn("node", [".\\node_modules\\webpack-dev-server\\bin\\webpack-dev-server.js"], {stdio: "inherit"});
     } else {
         webmlpolyfillHost = childprocess.spawn("./node_modules/webpack-dev-server/bin/webpack-dev-server.js", {stdio: "inherit"});
     }
